07d0ff82e002bcb3fabd0fd8ad39d86b1fd1e901
KoshaKz <viskas228228@gmail.com>
2026-01-19 09:54:49 +0100

    fix: Add loadConfig() and messageManager to onEnable()
    Configuration loading now happens in onEnable() after Folia detection,
    ensuring all registries are initialized before EasSerializers is used.

--- easyarmorstands-plugin/src/main/java/me/m56738/easyarmorstands/EasyArmorStandsPlugin.java
+++ easyarmorstands-plugin/src/main/java/me/m56738/easyarmorstands/EasyArmorStandsPlugin.java
@@ -1,51 +1,19 @@
     @Override
-    public void onLoad() {
-        Permissions.registerAll();
+    public void onEnable() {
+        new Metrics(this, 17911);
+        adventure = BukkitAudiences.create(this);
+        gizmos = BukkitGizmos.create(this);
 
-        instance = this;
-        EasyArmorStandsInitializer.initialize(this);
-        loader.load();
-
-        propertyTypeRegistry = new PropertyTypeRegistryImpl();
-        new DefaultPropertyTypes(propertyTypeRegistry);
-
-        ArmorStandElementType armorStandElementType = new ArmorStandElementType();
-        entityElementProviderRegistry = new EntityElementProviderRegistryImpl();
-        entityElementProviderRegistry.register(new ArmorStandElementProvider(armorStandElementType));
-        entityElementProviderRegistry.register(new SimpleEntityElementProvider());
-
-        menuSlotTypeRegistry = new MenuSlotTypeRegistryImpl();
-        menuSlotTypeRegistry.register(new EntityCopySlotType());
-        menuSlotTypeRegistry.register(new ArmorStandPartSlotType());
-        menuSlotTypeRegistry.register(new ArmorStandPositionSlotType());
-        menuSlotTypeRegistry.register(new ArmorStandSpawnSlotType(armorStandElementType));
-        menuSlotTypeRegistry.register(new BackgroundSlotType());
-        menuSlotTypeRegistry.register(new ColorAxisSlotType());
-        menuSlotTypeRegistry.register(new ColorAxisChangeSlotType());
-        menuSlotTypeRegistry.register(new ColorIndicatorSlotType());
-        menuSlotTypeRegistry.register(new ColorPickerSlotType());
-        menuSlotTypeRegistry.register(new ColorPresetSlotType());
-        menuSlotTypeRegistry.register(new DestroySlotType());
-        menuSlotTypeRegistry.register(new PropertySlotType());
-        menuSlotTypeRegistry.register(new FallbackSlotType(Key.key("easyarmorstands", "spawn/item_display")));
-        menuSlotTypeRegistry.register(new FallbackSlotType(Key.key("easyarmorstands", "spawn/block_display")));
-        menuSlotTypeRegistry.register(new FallbackSlotType(Key.key("easyarmorstands", "spawn/text_display")));
-        menuSlotTypeRegistry.register(new FallbackSlotType(Key.key("easyarmorstands", "spawn/interaction")));
-        menuSlotTypeRegistry.register(new FallbackSlotType(Key.key("easyarmorstands", "spawn/mannequin")));
-        menuSlotTypeRegistry.register(new FallbackSlotType(Key.key("easyarmorstands:traincarts/model_browser")));
-        menuSlotTypeRegistry.register(new FallbackSlotType(Key.key("easyarmorstands:headdatabase")));
-
-        MannequinCapability mannequinCapability = getCapability(MannequinCapability.class);
-        if (mannequinCapability != null) {
-            MannequinElementType<?> type = mannequinCapability.createElementType();
-            entityElementProviderRegistry.register(new MannequinElementProvider<>(type, mannequinCapability));
-            menuSlotTypeRegistry.register(new MannequinSpawnSlotType(type));
+        // Detect if running on Folia or Paper
+        try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+            getLogger().info("\u2705 [Folia Support] Running on Folia with multi-threaded scheduler");
+        } catch (ClassNotFoundException e) {
+            getLogger().info("\u2b55 [Folia Support] Running on Paper/Spigot with single-threaded scheduler");
         }
 
-        menuProvider = new MenuProviderImpl();
+        loadConfig();
+        messageManager = new MessageManager(this);
+        messageManager.load(config);
 
-        regionPrivilegeManager = new RegionListenerManager();
-
-        addonManager = new AddonManager(getLogger());
-        addonManager.load(getClassLoader());
-    }
+        loadProperties();
